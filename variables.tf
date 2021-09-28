1596b4d6f4b48ab05882d7d8ccbcdef3a4202868
Michael Ajanaku <remiljw@gmail.com>
2021-09-28 01:46:42 +0100

    change port to 5000

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "machine_image" {
 variable "instance_port" {
   description = "The port the EC2 Instance should listen on for HTTP requests."
   type        = number
-  default     = 8080
+  default     = 5000
 }
 
 variable "instance_type" {
